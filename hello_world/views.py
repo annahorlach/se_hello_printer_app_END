fe5805af0863af1331441a2a415b946763c7f648
aleksanderbuczek <aleksanderbuczek@users.noreply.github.com>
2020-08-19 12:41:15 +0200

    Final Travis 0

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,7 +3,7 @@ from hello_world.formater import get_formatted
 from hello_world.formater import SUPPORTED, PLAIN
 from flask import request
 
-moje_imie = "Aleksander Jerzy"
+moje_imie = "Aleksander"
 moje_nazwisko = "Buczek"
 msg = "Hello World!"
 
